e8ce5c4218639ee5975ad17f9b0cfd2f782cf926
Ebrahim <elsayedebrahim603@gmail.com>
2025-02-16 15:40:33 +0200

    feat: add authentication module with service, controller, routes, and validation; enhance server logging

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,8 +1,8 @@
 import app from "./app";
-import config from './config/env'
+import config from "./config/env";
 import logger from "./config/logger";
 
 
 app.listen(config.PORT, () => {
-  logger.info(`Server started running on PORT ${config.PORT}`)
+  logger.info(`Server started running on PORT ${config.PORT}`);
 });
