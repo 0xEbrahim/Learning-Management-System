1863f591169d6dc73cb4f528c03cd3761097251b
Ebrahim <elsayedebrahim603@gmail.com>
2025-02-19 14:39:42 +0200

    feat: add fs-extra dependency and implement swagger YAML file copying

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -5,9 +5,9 @@ import logger from "./config/logger";
 app.listen(config.PORT, () => {
   if (config.NODE_ENV === "production") {
     logger.info(`Server started running on PORT ${config.PORT}`);
-    logger.info("API Doc available at " + config.PROD_URL+"/api-docs");
+    logger.info("API Doc available at " + config.PROD_URL + "/api-docs");
   } else {
     console.log("Server started on port " + config.PORT);
-    console.log("API Doc available at " + config.DEV_URL+"/api-docs");
+    console.log("API Doc available at " + config.DEV_URL + "/api-docs");
   }
 });
