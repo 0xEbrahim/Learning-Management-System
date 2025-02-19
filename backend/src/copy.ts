4e2919ff5693ae044e956a62e3a4f180bafe2ea3
Ebrahim <elsayedebrahim603@gmail.com>
2025-02-19 14:43:10 +0200

    feat: update swagger YAML file and improve copy script formatting

--- backend/src/copy.ts
+++ backend/src/copy.ts
@@ -1,11 +1,11 @@
-import fs from 'fs-extra';
-import path from 'path';
+import fs from "fs-extra";
+import path from "path";
 
-const srcPath = path.join(__dirname,"..", 'src', 'swagger', 'swagger.yaml');
-const destPath = path.join(__dirname,"..", 'dist', 'swagger', 'swagger.yaml');
+const srcPath = path.join(__dirname, "..", "src", "swagger", "swagger.yaml");
+const destPath = path.join(__dirname, "..", "dist", "swagger", "swagger.yaml");
 
 fs.ensureDirSync(path.dirname(destPath));
 
 fs.copyFileSync(srcPath, destPath);
 
-console.log('Swagger YAML copied successfully.');
+console.log("Swagger YAML copied successfully.");
