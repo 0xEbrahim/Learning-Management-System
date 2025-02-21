faebb5351c01a61578bdb617edd5d69436d85450
boshraemad <boshraemad2004@icloud.com>
2025-02-22 00:18:11 +0200

    handle errors in register&&login

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,11 +3,13 @@ import Register from "./components/register.jsx"
 import { Route,Routes} from 'react-router-dom'
 import Login from './components/login.jsx'
 import VerifyEmail from "./components/verifyEmail.jsx";
+import HomePage from "./pages/homePage.jsx";
 function App() {
 
   return (
     <>
       <Routes>
+        <Route path="/homePage" element={<HomePage/>}/>
         <Route path="/" element={<Register/>}/>
         <Route path="/login" element={<Login/>}/>
         <Route path="/verifyEmail/:token" element={<VerifyEmail/>}/>
