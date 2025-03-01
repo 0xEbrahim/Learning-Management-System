a4fda8a64a515f7cd999ec0212f2030f62f4983c
Ebrahim <elsayedebrahim603@gmail.com>
2025-03-01 15:07:19 +0200

    fix: disable secure flag for cookies in Auth module for development environment

--- backend/src/modules/Auth/Auth.Controller.ts
+++ backend/src/modules/Auth/Auth.Controller.ts
@@ -29,13 +29,6 @@ export const register = asyncHandler(
 export const login = asyncHandler(
   async (req: Request, res: Response, next: NextFunction): Promise<void> => {
     const result: IResponse = await AuthService.login(req.body as ILoginBody);
-    if (result.statusCode === 403) {
-      res.cookie("email", req.body.email, {
-        maxAge: 7 * 24 * 60 * 60 * 1000,
-        secure: false,
-        httpOnly: true,
-      });
-    }
     sendResponse(result, res);
   }
 );
