fc0e20b28fc6895a7ca73d03df56dd6e6e977735
boshraemad <boshraemad2004@icloud.com>
2025-03-12 03:01:56 +0200

    fix: refresh accessToken && login

--- Frontend/src/axiosInstance.js
+++ Frontend/src/axiosInstance.js
@@ -1,8 +1,45 @@
-// import axios from "axios";
+import axios from "axios";
+import store from "./rtk/store";
+import { refreshAccessToken } from "./rtk/slices/authSlice";
 
-// const api = axios.create({
-//   baseURL: import.meta.env.VITE_API_URL, // Your API URL
-//   withCredentials: true, // Enable sending HTTP-only cookies
-// });
 
-// export default api;
+
+const apiUrl=import.meta.env.VITE_API_URL;
+const api = axios.create({
+  baseURL: apiUrl, // Your API URL
+  headers:{ "Content-Type": "application/json" }
+});
+//add accessToken if exists to api requests
+api.interceptors.request.use((config)=>{
+    //get accessToken from redux state
+    const state=store.getState();
+    const accessToken=state.auth.token;
+    if(accessToken){
+        config.headers.Authorization=`Bearer ${accessToken}`
+    }
+    return config;
+});
+
+api.interceptors.response.use((response)=>response,
+    async(error)=>{
+        const originalRequest=error.config;
+        if(error.response?.status === 403 && !originalRequest._retry){
+            originalRequest._retry=true;
+            
+            try{
+                const newAccessToken=await store.dispatch(refreshAccessToken());
+                if(newAccessToken){
+                    originalRequest.headers.Authorization = `Bearer ${newAccessToken}`;
+                    return axiosInstance(originalRequest);
+                }
+            }catch(error){
+                const state=store.getState();
+                state.auth.token=null;
+                return Promise.reject(error);
+            }
+        }
+        return Promise.reject(error);
+    }
+);
+
+export default api;
--- Frontend/src/rtk/slices/userSlice.js
+++ Frontend/src/rtk/slices/userSlice.js
@@ -1,42 +1,15 @@
-import { createAsyncThunk, createSlice} from "@reduxjs/toolkit";
-import axios from "axios";
-const apiUrl = import.meta.env.VITE_API_URL;
+import { createSlice} from "@reduxjs/toolkit";
 
-export const getUserDataById=createAsyncThunk(
-    "user/getData",
-    async ({ userId, accessToken }, { rejectWithValue }) => {
-        try {
-          const response = await axios.get(`${apiUrl}/users/${userId}`, {
-            headers: {
-              Authorization: `Bearer ${accessToken}`,
-              'Content-Type': 'application/json'
-            }
-          });
-          return response.data.data;
-        }catch(error){
-          if(error.response.status === 403){
-            return rejectWithValue("Session expired, please login again");
-          }
-          return rejectWithValue("cant get userData");
-        }
-    }
-);
 
 const userSlice=createSlice({
-    name:'userSlice',
-    initialState:{userData:null,error:null},
-    reducers:{},
-    extraReducers: (builder)=>{
-        builder
-        .addCase(getUserDataById.fulfilled,(state,action)=>{
-            state.userData=action.payload;
-            state.error=null;
-        })
-        .addCase(getUserDataById.rejected,(state,action)=>{
-            state.userData=null;
-            state.error=action.payload;
-        })
-    }
-})
+    name:"userSlice",
+    initialState:{userData:null},
+    reducers:{
+      storeUserData:(state,action)=>{
+        state.userData=action.payload;
+      }
+    },
 
+})
+export const {storeUserData}=userSlice.actions;
 export default userSlice.reducer;
