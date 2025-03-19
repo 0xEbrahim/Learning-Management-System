c37bba239e581972a0b220c01674d74c2967cd0e
Ebrahim <elsayedebrahim603@gmail.com>
2025-03-20 00:21:59 +0200

    refactor: remove unused authorization middleware from order routes

--- backend/src/modules/Order/Order.routes.ts
+++ backend/src/modules/Order/Order.routes.ts
@@ -1,6 +1,5 @@
 import express from "express";
 import isAuthenticated from "../../middlewares/isAuthenticated";
-import isAuthorized from "../../middlewares/isAuthorized";
 import {
   createCheckoutSession,
   getAllOrders,
