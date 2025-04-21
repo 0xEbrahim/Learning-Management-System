21b993c73df5a8fe6f528fa62b69fe32c4e96601
Ebrahim <elsayedebrahim603@gmail.com>
2025-04-22 01:55:19 +0200

    fix(socket): correct typo in notification data interface

--- backend/src/Sockets/notifications.socket.ts
+++ backend/src/Sockets/notifications.socket.ts
@@ -1,19 +1,22 @@
 import { DefaultEventsMap, Socket } from "socket.io";
 import prisma from "../config/prisma";
-import { IAddReviewNotificanData } from "../Interfaces/types";
+import { IAddReviewNotificationData } from "../Interfaces/types";
 
 export default (
   socket: Socket<DefaultEventsMap, DefaultEventsMap, DefaultEventsMap, any>
 ) => {
-  socket.on("addReviewNotification", async (data: IAddReviewNotificanData) => {
-    const { authorId, courseName, reviewId } = data;
-    const text = `You have a new Review at ${courseName} course`;
-    const notification = await prisma.notification.create({
-      data: {
-        recieverId: authorId,
-        text: text,
-      },
-    });
-    socket.to(authorId).emit("notificationSent", { notification, reviewId });
-  });
+  socket.on(
+    "addReviewNotification",
+    async (data: IAddReviewNotificationData) => {
+      const { authorId, courseName, reviewId } = data;
+      const text = `You have a new Review at ${courseName} course`;
+      const notification = await prisma.notification.create({
+        data: {
+          recieverId: authorId,
+          text: text,
+        },
+      });
+      socket.to(authorId).emit("notificationSent", { notification, reviewId });
+    }
+  );
 };
