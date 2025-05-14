2a015436bacf270875d0f4d0da783ab49f6a3a27
Ebrahim <elsayedebrahim603@gmail.com>
2025-05-14 18:27:22 +0300

    refactor routes to improve code organization and readability

--- backend/src/modules/User/User.routes.ts
+++ backend/src/modules/User/User.routes.ts
@@ -13,9 +13,7 @@ import { getUserByIdValidation, updateUserValidation } from "./User.validation";
 import uplaoder from "../../config/multer";
 import { orderRouter } from "../Order/Order.routes";
 const router = express.Router({ mergeParams: true });
-
 router.use("/:userId/orders", orderRouter);
-
 router.get("/", isAuthenticated, getUsers);
 router.get("/search", isAuthenticated, search);
 router.get("/:id", isAuthenticated, validate(getUserByIdValidation), getUser);
