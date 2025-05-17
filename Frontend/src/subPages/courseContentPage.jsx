6aeacbd1fe4fef54bdbb2b4d157c080f9bf64d64
boshraemad <boshraemad2004@icloud.com>
2025-05-17 17:25:30 +0300

    add delete video feature

--- Frontend/src/subPages/courseContentPage.jsx
+++ Frontend/src/subPages/courseContentPage.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { useSelector } from "react-redux";
 import CourseSection from "../components/courseSections";
 import { MdAddBox } from "react-icons/md";
 import api from "../axiosInstance";
@@ -8,6 +9,8 @@ function CourseContent(){
     const [showForm,setShowForm]=useState(false);
     const [sectionNameInput,setSectionNameInput]=useState("");
     const {courseId}=useParams();
+    const userId=useSelector((state)=>state.user.userData?.id);
+    const authorId=useSelector((state)=>state.authorId?.id);
     //show sweet-alert once section added successfully
     const addSection=async()=>{
         try{
@@ -41,7 +44,7 @@ function CourseContent(){
                                         <input className="bg-gray-50 border-1 border-gray-300 rounded-sm outline-none mt-2 px-2 py-1 text-sm" placeholder="add section" value={sectionNameInput} onChange={(e)=>{setSectionNameInput(e.target.value)}}/>
                                         <button onClick={()=>{addSection()}} className="px-2 py-1 bg-indigo-600 text-white rounded-sm text-[12px] font-[500] cursor-pointer ms-2">add</button>
                                         <button onClick={()=>{setShowForm(false)}} className="px-2 py-1 bg-gray-200 text-indigo-600 rounded-sm text-[12px] font-[500] cursor-pointer ms-1">cancel</button>
-                                    </div> :  <MdAddBox onClick={()=>{setShowForm(true)}} className=" add-section cursor-pointer text-2xl text-indigo-600 mb-3"/>}
+                                    </div> :   userId === authorId && <MdAddBox onClick={()=>{setShowForm(true)}} className=" add-section cursor-pointer text-2xl text-indigo-600 mb-3"/>}
             <div className="sections">
             <CourseSection/>
             </div>
